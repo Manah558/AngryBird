6033dc37fe81ee6a6eecc3fccbdd1e85f01c9802
Manah558 <konaurp@gmail.com>
2021-03-01 12:18:00 +0600

    test

--- BaseClass.js
+++ BaseClass.js
@@ -13,6 +13,7 @@ class BaseClass{
       }
       display(){
         var angle = this.body.angle;
+        
         push();
         translate(this.body.position.x, this.body.position.y);
         rotate(angle);
